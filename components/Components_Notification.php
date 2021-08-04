fbcefd4ef7c43d912a5f76f11627118d1daee896
claudio_966 <claudiomatheus966@gmail.com>
2021-08-04 12:14:24 -0300

    Método verify_db_table() adicionado

--- components/Components_Notification.php
+++ components/Components_Notification.php
@@ -3,7 +3,7 @@ require __DIR__.'/send.php';
 class Components_Notification {
 	public $amount_memories;
 	public $html;
-	// Verify the memories presents on database //
+	// Check the memories presents on database //
 	public function get_memories (){
 		$config_db = parse_ini_file(__DIR__ . '/database.ini');	
 		$connection = mysqli_connect($config_db['localhost'], $config_db['username'], $config_db['password'], 
@@ -17,14 +17,21 @@ class Components_Notification {
 			$list_memories[$item_memory['ID']]['CAPACITY'] = $item_memory['CAPACITY'];
 		}
 		$count_memories = count($list_memories);
-		// TODO Desmembrar as condições em dois if's //
-		if ($count_memories > $this->amount_memories || $count_memories < $this->amount_memories) {
+		if ($count_memories > $this->amount_memories) {
 			$this->amount_memories = $count_memories;
+			$this-verify_db_table($connection, $this->amount_memories, "memory");
 			$this->get_html_general_information($list_memories);
 			Send_Email($this->html);  // The notification email will be send 
+		} else {
+			if ($count_memories < $this->amount_memories)
+			$this->amount_memories = $count_memories;
+			$this->get_html_general_information($list_memories);
+			Send_Email($this->html);  // The notification email will be send
 		}
+			$connection->close();
 	}
 
+	// Generate the html body for email //
 	public function get_html_general_information($list_memories) {
 		$this->html = "<h1> Novos componentes de hardware foram adicionados <h1><br><h2>Informações da memória<h2><br>";
 		foreach ($list_memories as $memory) {
@@ -32,6 +39,39 @@ class Components_Notification {
 				if ($value == "Unknown") continue;
 				$this->html .= " - $value<br>";
 			}
+		}
+	}
+
+	// Check whether components table exists on database //	
+	private function verify_db_table($connection, $amount_memories, $component) {
+		$table = 'hardware_components'
+		$sql_create_table = "CREATE TABLE hardware_components (component_id INT NOT NULL AUTO_INCREMENT, name VARCHAR(40), amount INT NOT NULL, PRIMARY KEY ( id_component )";
+		$sql_insert_value = "INSERT INTO hardware_components (name, amount) VALUES ('$component', '$amount_memories')";
+		$sql_update_value = "UPDATE hardware_components SET amount=$amount_memories WHERE name=$component"
+		$query_table_result = mysqli_query("SHOW TABLES LIKE '$table'");
+		$query_value_result = mysqli_query("SELECT component_id FROM hardware_components WHERE name = '$component'");
+
+		$query_table_result = $result && $result->num_rows > 0;
+		// check if table exist //
+		if ($query_table_result) {
+			// check if the entry exist //
+			if ($query_value_result) {
+				if ($connection->query($sql_update_value) === TRUE)
+					echo "Value has been update sucessfully\n";
+				else 
+					echo "Value hasn't been update" . $connection->error;
+			} else {
+				if ($connection->query($sql_insert_value) === TRUE) 
+					echo "Value has been insert sucessfully\n";
+				else 
+					echo "Value hasn't been insert" . $connection->error;
+
+			}		
+		} else {		
+			if ($connection->query($sql_create_table) === TRUE)
+				echo "Table has been create sucessfully\n";
+			else 
+				echo "Don't hasn't been create" . $connection->error;
 		}
 	}
 }
