e9dab02e28595d4e1b6541e8413f9cc13379c9d8
claudio_966 <claudiomatheus966@gmail.com>
2021-08-17 11:34:20 -0300

    função get_htmL_general_information modifica. Designação para alteração de um hardware definida

--- components/Components_Notification.php
+++ components/Components_Notification.php
@@ -21,59 +21,91 @@ class Components_Notification {
 		$result_hardware = mysqli_query($connection, $sql);
 		$hardware_result = mysqli_fetch_array($result_hardware);
 					
-		if ($count_memories > $hardware_result['amount']) {
-			$this->get_html_general_information($list_memories, $hardware_result, $connection);
+		if ($count_memories > 0) {
+			$this->get_html_general_information($list_memories, $hardware_result, $connection, $is_additon = true);
 			//Send_Email($this->html);  // The notification email will be send 
 			$sql = "UPDATE hardware_components SET amount = $count_memories WHERE name = 'memories'";
 			mysqli_query($connection, $sql);
-			echo $this->html;
 		} else {
 			if ($count_memories < $hardware_result['amount']) {
-				$this->amount_memories = $count_memories;
-				$this->get_html_general_information($list_memories, $hardware_result, $connection);
+				$this->get_html_general_information($list_memories, $hardware_result, $connection, $is_addition = false);
+				$sql = "UPDATE hardware_components SET amount = $count_memories WHERE name = 'memories'";
 				//Send_Email($this->html);  // The notification email will be send
 			}
-		}	
+		}
 			$connection->close();
 	}
 
 	// Generate the html body for email //
-	public function get_html_general_information($list_memories, $hardware_result, $connection) {
-		$this->html .= "
-			<hr>
-			<center>
-				<h1> Novos Componentes de Hardware Adicionados <h1>
-			</center>
-			<hr>
-			<center>
-				<h2> Memories <h2>	
-				<table border='1'>
-				<tr>\n";
-		$reference_array = '1';
-		foreach ($list_memories[$reference_array] as $label => $value) {
-				$this->html .= "<th>$label</th>\n";
-		} 
-		$this->html .= "</tr>\n<tr>";
-		
-		for ($i = $hardware_result['amount']; $i < count($list_memories); $i++) {
-			foreach ($list_memories["$i"] as $feature => $value) {
-				if ($value == 'Unknown' or $value == '') { 
-					$this->html .= "<td style='text-align:center'> Não Informado </td>\n";
+	public function get_html_general_information($list_memories, $hardware_result, $connection, $is_addition) {
+		if ($is_addition) {
+			$this->html .= "
+				<hr>
+				<center>
+					<h1> Novos Componentes de Hardware Adicionados <h1>
+				</center>
+				<hr>
+				<center>
+					<h2> Memories <h2>	
+					<table border='1'>
+					<tr>\n";
+			$reference_array = '1';
+			foreach ($list_memories[$reference_array] as $label => $value) {
+					$this->html .= "<th>$label</th>\n";
+			} 
+			$this->html .= "</tr>\n<tr>";
+			
+			for ($i = 1; $i <= array_key_last($list_memories); $i++) {
+				if (!array_key_exists($i, $list_memories)) {
 					continue;
 				}
-				if ($feature == "HARDWARE_ID") {
-					$sql = "SELECT userid FROM hardware WHERE id = '$value'";
-					$result_id = mysqli_query($connection, $sql);
-					$id = mysqli_fetch_array($result_id);	
-					$this->html .= "<td style='text-align:center'>" . $id['userid'] . "</td>\n";
-				} else {
-					$this->html .= "<td style='text-align:center'>$value</td>\n";
+				foreach ($list_memories["$i"] as $feature => $value) {
+					if ($value == 'Unknown' or $value == '') { 
+						$this->html .= "<td style='text-align:center'> Não Informado </td>\n";
+						continue;
+					}
+					if ($feature == "HARDWARE_ID") {
+						$sql = "SELECT userid FROM hardware WHERE id = '$value'";
+						$result_id = mysqli_query($connection, $sql);
+						$id = mysqli_fetch_array($result_id);	
+						$this->html .= "<td style='text-align:center'>" . $id['userid']."</td><td style='text-align:center'> Adicionado </td>\n";
+					} else {
+						$this->html .= "<td style='text-align:center'>$value</td>\n";
+					}
+				}
+				$this->html .= "</tr><tr>";
+			}
+				$this->html .= "</table>";
+		} else {
+			$this->html .= "
+				<br>
+				<hr>
+				<center>
+					<h1> Novos Componentes de Hardware Retirados <h1>
+				</center>
+				<hr>
+				<center>
+					<h2> Memories <h2>	
+					<table border='1'>
+					<tr>\n";
+
+			$reference_array = '1';
+			foreach ($list_memories[$reference_array] as $label => $value) {
+					$this->html .= "<th>$label</th>\n";
+			}
+			$this->html .= "</tr>\n<tr>";
+			
+			for ($i = 1; $i <= array_key_last($list_memories); $i++) {
+				if (!array_key_exists($i, $list_memories)) {
+					$this->html .= "<td style='text-align:center'> Removido </td>\n";	
+					$this->html .= "</tr><tr>\n";
 				}
 			}
-			$this->html .= "</tr><tr>";
+				$this->html .= "</table>";
+		
 		}
-			$this->html .= "</table>";
 	}
+
 }
 ?>
 
