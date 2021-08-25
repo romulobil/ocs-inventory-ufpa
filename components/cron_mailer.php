6c1b17bb93d81bc50efe03c36ded56e4d88aa5b0
claudio_966 <claudiomatheus966@gmail.com>
2021-08-24 22:10:08 -0300

    Envio de e-mail centralizado no cron_mailer e adição da chamada do método get_monitors()

--- components/cron_mailer.php
+++ components/cron_mailer.php
@@ -1,5 +1,14 @@
 <?php
-require __DIR__.'/Components_Notification.php';
+require __DIR__ . '/Components_Notification.php';
+require_once( __DIR__ . '/send.php');
+
 	$obj = new Components_Notification();
 	$obj->get_memories();
+	$obj->get_monitors();
+	
+	$body_mail = $obj->html_part_addition . $obj->html_part_remove;
+	if ($body_mail != '')
+		Send_Email($body_mail);
+	else 
+		echo "Nada a enviar :(" . "\n";	
 ?>
