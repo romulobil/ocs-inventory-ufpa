71e3c5bfd84e00aef5ad69b8a461e2fcbbf8b043
claudio_966 <claudiomatheus966@gmail.com>
2021-08-29 12:01:36 -0300

    Adicionado a chamada do método get_storages()

--- components/cron_mailer.php
+++ components/cron_mailer.php
@@ -5,7 +5,8 @@ require_once( __DIR__ . '/send.php');
 	$obj = new Components_Notification();
 	$obj->get_memories();
 	$obj->get_monitors();
-	$obj->get_videos();	
+	$obj->get_videos();
+	$obj->get_storages();	
 
 	$body_mail = $obj->html_part_addition . $obj->html_part_remove;
 	if ($body_mail != '')
