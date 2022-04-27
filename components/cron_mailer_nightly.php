ed86cbd97df596c11120dc826a689319886a9af5
claudio_966 <claudiomatheus966@gmail.com>
2022-04-27 10:33:38 -0300

    cron_mailer_nightly adicionado

---
+++ components/cron_mailer_nightly.php
@@ -0,0 +1,22 @@
+<?php
+
+// Support the methods to collect hardware informations
+require_once __DIR__ . '/ComponentsNotification_nightly.php';
+// Support the functions to send an email
+require_once __DIR__ . '/send.php';
+
+	$mastermind = new ComponentsNotification();
+	
+	$mastermind->get_memories();
+	$mastermind->get_monitors();
+	$mastermind->get_videos();
+	$mastermind->get_storages();	
+	$mastermind->get_cpus();	
+	//$mastermind->update_id_assets();
+	// html_part_addition e html_part_remove are public attributes of ComponentesNotification
+	$body_mail = $mastermind->html_part_addition . $mastermind->html_part_remove;
+	if ($body_mail != '')
+		Send_Email($body_mail);
+	else 
+		echo "Nada a enviar :(" . "\n";	
+	
